15ead35f77d389f3f47e851ae22fe5064149e61d
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-28 01:26:42 +0200

    Show login when not logged and when logged in show logout  +  styling css

--- header.php
+++ header.php
@@ -6,5 +6,18 @@
             <a href="<?php echo $base_url; ?>/index.php">Home</a> |
             <a href="<?php echo $base_url; ?>/logs/index.php">Logs</a>
         </nav>
+        <div>
+        <div class="space1">
+            <?php
+            error_reporting(E_ERROR | E_WARNING | E_PARSE); 
+            session_start();
+            if(isset($_SESSION['user_id'])): ?>
+                <?php echo "<div class='space'>hello, <strong>", $_SESSION["user_name"], "</strong></div>"?>
+                <p><a href="<?php echo $base_url; ?>/login.php">Inloggen</a></p>
+            <?php else: ?>
+                <p><a href="<?php echo $base_url; ?>/logout.php">Uitloggen</a></p>
+            <?php endif; ?>
+            </div>
+        </div>
     </div>
 </header>
