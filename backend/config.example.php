a262318e64b1393ddae469615f4d52ef8acfb6cb
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-28 00:43:34 +0200

    Changed directory to my path

--- backend/config.example.php
+++ backend/config.example.php
@@ -12,4 +12,4 @@ $dbUser = 'root';
 $dbPass = '';
 
 //De url waarop jouw project staat. Géén slash aan het einde.
-$base_url = 'http://localhost/H6-timesheet';
+$base_url = 'http://localhost/Tweede%20Periode/H13_Timesheed';
