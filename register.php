6034697376514b6aa67969e1f168602bce52d600
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-29 09:04:46 +0200

    Builded a website for register.php

---
+++ register.php
@@ -0,0 +1,48 @@
+<?php session_start(); ?>
+
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>TimeSheet</title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+
+    <?php require_once 'header.php'; ?>
+    
+    <div class="container">
+
+        <h1>TimeSheet / Registere</h1>
+        <?php
+        if(isset($_GET['msg']))
+        {
+            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
+        }
+        ?>
+
+        <form action="backend/loginController.php" method="POST">
+            <div class="form-group">
+                <label for="username">Gebruikersnaam:</label>
+                <input type="text" name="username" id="username" placeholder="gebruikers naam">
+            </div>
+            <div class="form-group">
+                <label for="username">Naam:</label>
+                <input type="text" name="username" id="username" placeholder="Gebruik je naam">
+            </div>
+            <div class="form-group">
+                <label for="password">Wachtwoord:</label>
+                <input type="password" name="password" id="password" placeholder="password">
+            </div>
+            <div class="form-group">
+                <label for="passwordRetry">Wachtwoord opnieuw:</label>
+                <input type="password" name="passwordRetry" id="passwordRetry" placeholder="password opnieuw">
+            </div>
+            <input type="submit" value="Registreer">
+        </form>
+    </div>
+
+</body>
+
+</html>
