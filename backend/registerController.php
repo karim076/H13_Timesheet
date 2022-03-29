c81ae3febf34760cc297584b410855da8157fcf8
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-29 09:45:48 +0200

    Added if password is equal passwordRetry

--- backend/registerController.php
+++ backend/registerController.php
@@ -1,8 +1,23 @@
 <?php
-if(isset($_SESSION['user_id']))
-{
-    die("Kan niet registreren je bent al ingelogd");    
-}
-
-
+    session_start();
+    if(isset($_SESSION['user_id']))
+    {
+        die("Kan niet registreren je bent al ingelogd");    
+    }
+    $username = $_POST['username'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $passwordRetry = $_POST['passwordRetry'];
+    # kijkt of het email geldig is
+    if(filter_var($email,FILTER_VALIDATE_EMAIL) === false)
+    {
+        die('Email is ongeldig!');
+    }
+    # hashing
+    if ($password != $passwordRetry)
+    {
+        die()
+    }
+    $hash = password_hash($password)
 ?>
